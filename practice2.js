b9bc856daf0ca0ec24b4498b39f9b125e60eb093
Marco Mendão <mac.mendao@gmail.com>
2022-06-20 20:40:47 +0100

    Add practice2

---
+++ practice2.js
@@ -0,0 +1,50 @@
+const inquirer = require('inquirer')
+const MongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url)
+client.connect()
+
+// Listar Contactos
+// Criar contactos
+// Apagar contactos
+
+function options(){
+
+    const questions = [
+        {
+            type: "input",
+            name: "contactList",
+            message: "Type a contact list"
+        },
+        {
+            type: "rawlist",
+            name: "action",
+            message: "Choose an action",
+            choices: ["List Contacts", "Create Contact", "Remove Contact", "Exit"]
+        }
+    ]
+
+    inquirer.prompt(questions).then(function (answers){
+        const contactList = answers.contactList
+        const action = answers.action
+
+        switch(action){
+            case "List Contacts":
+                // listContacts()
+                break;
+            case "Create Contact":
+               // createContact()
+                break;
+            case "Remove Contact":
+                // removeContacts()
+                break;
+            case "Exit":
+                process.exit()
+            default:
+                options()
+        }
+    })
+}
+
+options()
