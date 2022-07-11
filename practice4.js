32cbd38f670b333424965033f49e57382849e822
Marco Mendão <mac.mendao@gmail.com>
2022-07-11 20:29:01 +0100

    add practice4.js

---
+++ practice4.js
@@ -0,0 +1,21 @@
+// 1. Create an interface that asks the user to add a contact and ask organization info as well
+// (Use normalize data model)
+// 2. Create an interface that shows the total number of contacts by organization
+
+// Company, Total
+// MoOngy, 100
+// Betacode, 10
+// Auchan, 3
+
+// 3. Create an interface that list all contact numbers of one organization
+
+// Company, Numbers
+// MoOngy, ["+351 54634566345", "+351 455325324"]
+
+// 1. Training aggregate
+// 2. Training operation $group
+// 2.1 Training $sum
+// 2.2 Training $push or $addToSet
+// 3. Training $lokup
+
+// 4. Create an interface that list all contacts with organization info
